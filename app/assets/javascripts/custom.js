9d4e3bef17b3bb03b01a006c45ea9bca48769be4
Joshua Ledbetter <ledbetterljoshua@gmail.com>
2015-02-01 09:09:11 -0600

    added forward and backward buttons to posts

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -11,3 +11,19 @@ function bannerImg() {
 var imageSrc = $("#theHiddenImg img").attr("src")
 $("div#banner").attr("style", "background-image: url(\" " + imageSrc + "\");")
 }
+
+function postControls() {
+	var currnetLocation = location.pathname;
+	var next = $("a.download-btn.next").attr("href");
+	var last = $("a.download-btn.last").attr("href");
+	var first = $("a.download-btn.first").attr("href");
+	$("a.download-btn.last").hide();
+
+	if (currnetLocation == last) {
+	 $("a.download-btn.next").hide();
+	 $("a.download-btn.first").show();
+	} else if ( currnetLocation == first) {
+	 $("a.download-btn.next").show();
+	 $("a.download-btn.first").hide();
+	}
+}
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,21 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "400x400>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+	def previous
+  Post.where(["id < ?", id]).order(:id).last
+	end
+
+	def next
+  Post.where(["id > ?", id]).order(:id).first
+	end
+
+	def last 
+		Post.order(:id).last
+	end
+
+	def first
+Post.order(:id).first
+	end
+
 end
