0307297a93c0f5e1d86a54fbb72873480ebb2aea
Joshua Ledbetter <ledbetterljoshua@gmail.com>
2015-02-01 19:48:43 -0600

    made header scroll friendly

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -27,3 +27,18 @@ function postControls() {
 	 $("a.download-btn.first").hide();
 	}
 }
+
+var lastScrollTop = 0;
+$(window).scroll(function(event){
+   var st = $(this).scrollTop();
+   if (st > lastScrollTop){
+       $("nav.navbar.navbar-default").attr("style", "margin-top:-74px;");
+   } else {
+      $("nav.navbar.navbar-default").attr("style", "margin-top:0px;");
+   }
+   lastScrollTop = st;
+      if (document.body.scrollTop === 0) {
+     $("nav.navbar.navbar-default").attr("style", "margin-top:0px;transition:all 0 ease-in");
+   }
+});
+
