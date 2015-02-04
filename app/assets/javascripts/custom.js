28a22c655ef8c1ed451be163c34fbf41e23e6b60
Joshua Ledbetter <ledbetterljoshua@gmail.com>
2015-02-03 22:23:36 -0600

    redesigned header scroll function so it would work on firefox

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -28,7 +28,7 @@ function postControls() {
 	} 
 }
 
-var lastScrollTop = 0;
+/*var lastScrollTop = 0;
 $(window).scroll(function(event){
    var st = $(this).scrollTop();
    if (st > lastScrollTop){
@@ -40,5 +40,45 @@ $(window).scroll(function(event){
       if (document.body.scrollTop === 0) {
      $("nav.navbar.navbar-default").attr("style", "margin-top:0px;transition:all 0 ease-in");
    }
+});*/
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('.navbar').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
 });
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('.navbar').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('.navbar').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
 
